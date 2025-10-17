cc911b8e88207c19e385fa6db1ae188d86caeace
WouterFeijen <wfeijen100@gmail.com>
2025-10-17 23:46:04 +0200

    implemented PCM in possibilistic_c_means

--- algorithms/possibilistic_c_means.py
+++ algorithms/possibilistic_c_means.py
@@ -1,32 +1,35 @@
 import numpy as np
 from base import BaseClustering
+from fuzzy_c_means import FCM
 
-class FCM(BaseClustering):
+class PCM(BaseClustering):
 
     def __init__(self,c,m=2,distance_measure="euclidean",max_iter=100,epsilon=1e-5):
         
-        self.c = c # nr of clusters
-        self.m = m # fuzziness parameter
-        self.distance_measure = distance_measure
+        self.nr_clusters = c
+        self.m = m
+        self.dist_measure = distance_measure
         self.max_iter = max_iter
-        self.epsilon = epsilon # tolerance to declare convergence
+        self.epsilon = epsilon
 
     def fit(self,X):
         """Assign possibilities to all data points in X for each 
         cluster K and set self.U_"""
         
-        N = X.shape[0]
+        # init U and V using FCM 
+        FCM_clustering = FCM(c=self.nr_clusters,m=self.m)
+        U = FCM_clustering.fit_predict(X)
+        V = FCM_clustering.V_
 
-        # init U0
-        U = self.init_u0(self.c,N)
+        # estimate eta_k's
+        self.eta = self.get_eta(U,V,X)
 
-        # iterate until convergence or max_iter
         for _ in range(self.max_iter):
-            # compute cluster centers
-            V = self.get_cluster_centers(U,X)
+            # update cluster prototypes
+            V = self.update_V(U,X)
 
-            # update membership matrix
-            U_new = self.update_U(X,V)
+            # compute new memberships
+            U_new = self.update_U(V,X)
 
             # check convergence
             if np.max(abs(U - U_new)) < self.epsilon:
@@ -34,9 +37,9 @@ class FCM(BaseClustering):
                 break
             else:
                 U = U_new
-
+        
         self.U_ = U
-        self.V_ = self.get_cluster_centers(self.U_, X)# V
+        self.V_ = self.update_V(self.U_,X)
         return self
 
     def predict(self,X):
@@ -44,57 +47,61 @@ class FCM(BaseClustering):
         if not hasattr(self, "U_") or not hasattr(self, "V_"):
             raise ValueError("Model has not been fit yet. Please call 'fit' before 'predict'.")
         
-        return self.update_U(X,self.V_)
+        return self.update_U(self.V_,X)
 
     def fit_predict(self,X):
         """Fit the model to X and return the fuzzy membership values for all data points."""
         self.fit(X)
         return self.U_
 
-    def init_u0(self,c,N,rndm=True):
-        """Initialize the c x N membership matrix where each column k represents a data point and
-        each element U_ik is the membership degree of point k to cluster i each column k must 
-        sum to 1"""
-        if rndm:
-            U = np.random.rand(c,N)
-            return U / np.sum(U,axis=0,keepdims=True)
-        else:
-            raise NotImplementedError("confusion in the original paper about a fixed initialization")
-        
-    def get_cluster_centers(self,U,X):
-
-        Um = U ** self.m
-        numerator = Um @ X # (c x N) @ (N x d) => (c x d)
-        denominator = Um.sum(axis=1, keepdims=True)
-        return numerator / denominator
-
-    def update_U(self,X,V,A=None):
-
-        # get squared distances
-        d2 = self.a_norm_distances(X=X,V=V,A=A)
-        d2 = np.fmax(d2,1e-10) # avoid division by zero
-        
-        exponent = 1.0 / (self.m-1) # use 1.0 instead of 2.0 bc distances are squared
-        
-        # get distance ratios
-        ratio = (d2[:,None,:] / d2[None,:,:]) ** exponent
-        
-        # sum and take inverse to get memberships
-        return 1.0 / np.sum(ratio, axis=1)
+    def get_eta(self,U,V,X,R=1):
     
-    def a_norm_distances(self,X,V,A=None):
+        Um = U ** self.m
+        
+        D2 = self.a_norm_distances(X,V,distance_measure=self.dist_measure)
+
+        numerator = np.sum(
+            np.multiply(Um,D2),
+            axis=1)
+
+        denominator = Um.sum(axis=1)
+        
+        
+        return R * numerator / denominator
+
+
+    def a_norm_distances(self,X,V,A=None,distance_measure="euclidean"):
 
         if A is None:
-            if self.distance_measure == "euclidean":
+            if distance_measure == "euclidean":
                 A = np.eye(X.shape[1])
-            elif self.distance_measure == "diagonal":
+            elif distance_measure == "diagonal":
                 A = np.diag(1 / np.var(X, axis=0))
-            elif self.distance_measure == "mahalanobis":
+            elif distance_measure == "mahalanobis":
                 A = np.linalg.inv(np.cov(X, rowvar=False))
             else:
-                raise ValueError(f"Unknown distance measure: '{self.distance_measure}'")
+                raise ValueError(f"Unknown distance measure: '{distance_measure}'")
         
         # N.shape: (N,d) - V.shape: (c,d) -> matrix of differences (c,N,d)
         diff = X[None,:,:] - V[:,None,:] # vector diffs y_k - v_i
         # (c,N,d) @ (d,d) @ (c,N,d) -> (c,N)
         return np.einsum("cnd,df,cnf->cn",diff, A, diff)
+
+    def update_U(self,V,X):
+
+        D2 = self.a_norm_distances(X,V,distance_measure=self.dist_measure)
+        denominator = 1 + (D2 / self.eta[:,None]) ** (1/(self.m-1))
+
+        return 1 / denominator
+
+
+    def update_V(self,U,X):
+
+        Um = U ** self.m
+        numerator = Um @ X
+        denominator = Um.sum(axis=1,keepdims=True)
+
+        return numerator / denominator
+
+
+
